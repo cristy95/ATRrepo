6aca73e9c88acc87effeac2fc6122bdd81e65e2a
Kring2 <cristylorainefuerzas@gmail.com>
2014-08-16 13:41:06 +0800

    fixing problems

--- scripts/gethonstudperid.py
+++ scripts/gethonstudperid.py
@@ -5,7 +5,7 @@ import json
 def index(req, stud_id):
 	##cgi.escape(self)
 	a = doSql()
-	studs = a.execqry("select * from get_hon_stud_perid(" + stud_id + ");", False)
+	studs = a.execqry("select * from get_hon_stud(" + stud_id + ");", False)
 	result = []
 
 	for stud in studs:
