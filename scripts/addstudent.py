4a3a53a4262f0e7a65b5a46204036291157c6c37
Kring2 <cristylorainefuerzas@gmail.com>
2014-09-05 22:46:06 +0800

    checked files for adding students[working]

--- scripts/addstudent.py
+++ scripts/addstudent.py
@@ -2,7 +2,7 @@ from dosql import *
 import cgi
 import json
 
-def addStudent(req, stud_id, college, course, nameFirst, nameMid, nameLast, nickname, gender, birthdate, birthMonth, birthYear, age, contactNum, homeAddress):
+def index(req, stud_id, college, course, nameFirst, nameMid, nameLast, nickname, gender, birthdate, birthMonth, birthYear, age, contactNum, homeAddress):
   stud_id = cgi.escape(stud_id)
   college = cgi.escape(college)
   course = cgi.escape(course)
@@ -18,10 +18,10 @@ def addStudent(req, stud_id, college, course, nameFirst, nameMid, nameLast, nick
   contactNum = cgi.escape(contactNum)
   homeAddress = cgi.escape(homeAddress)
   x = doSql()
-  student = x.execqry("select * from get_stud_info_perid('" + stud_id + "', '" + college + "', '" + course + "', '" + nameFirst + "', '" + nameMid + "', '" + nameLast + "','" + nickname + "','" + gender + "','" + birthdate + "','" + birthMonth + "', '" + birthYear + "','" + age + "','" + contactNum + "','" + homeAddress + "',);", False)
+  student = x.execqry("select * from regstudent('" + stud_id + "', '" + college + "', '" + course + "', '" + nameFirst + "', '" + nameMid + "', '" + nameLast + "', '" + nickname + "', '" + gender + "', " + birthdate + ", " + birthMonth + ", " + birthYear + ", '" + age + "', '" + contactNum + "', '" + homeAddress + "');", True)
   result = []
-  for student in students:
-    stringed = map(str, student)
+  for stud in student:
+    stringed = map(str, stud)
     result.append(stringed)
 
   return json.dumps(result)
