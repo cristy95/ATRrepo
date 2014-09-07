b1c483d1ac610674c3a4bc802e34982a41f8fb47
thundervoice <dmbbandola@gmail.com>
2014-09-08 02:14:49 +0800

    modified getconfirmlist.py

--- scripts/getconfirmlist.py
+++ scripts/getconfirmlist.py
@@ -5,7 +5,7 @@ import json
 def index(req, status):
     status = cgi.escape(status)
     y = doSql()
-    confirm1 = y.execqry("select * from getconfirmed('"+ status + "');", False)
+    confirm1 = y.execqry("select * from getconfirm('"+ status + "');", False)
     result1 = []
     for cons in confirm1:
         stringed = map(str, cons)
