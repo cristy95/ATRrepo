adc14c7667a3cc58825b0e2ca409d2b5ab2cf583
thundervoice <dmbbandola@gmail.com>
2014-09-07 21:31:10 +0800

    added addconfirm.py

---
+++ scripts/addconfirm.py
@@ -0,0 +1,16 @@
+from dosql import *
+import cgi
+import json
+
+def index(req, stud_id):
+    stud_id =  cgi.escape(stud_id)
+    x = doSql()
+
+    stud = x.execqry("select * from setconfirm('" + stud_id + "');", True)
+    result = []
+
+    for stud in studs:
+        stringed = map(str, stud)
+        result.append(stringed)
+
+    return json.dumps(result)
