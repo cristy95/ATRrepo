12c7290f656cb444684e66b72c0e66ad3fc82599
Kring2 <cristylorainefuerzas@gmail.com>
2014-09-10 06:28:35 +0800

    checked fill2 [still not working]

--- javascript/globs.js
+++ javascript/globs.js
@@ -338,32 +338,27 @@ function addparents()
    $.ajax({
        
 	   url: siteloc + scriptloc + "addparents.py",
-       data: {stud_id:stud_id},
+       data: {stud_id:$("#stud_id").val(),
+		father_nameFirst:$("#father_nameFirst").val(),
+		father_nameMiddle:$("#father_nameMiddle").val(),
+		father_nameLast:$("#father_nameLast").val(),
+		mother_nameFirst:$("#mother_nameFirst").val(),
+		mother_nameMiddle:$("#mother_nameMiddle").val(),
+		mother_nameLast:$("#mother_nameLast").val(),
+		guardian_nameFirst:$("#guardian_nameFirst").val(),
+		guardian_nameMiddle:$("#guardian_nameMiddle").val(),
+		guardian_nameLast:$("#guardian_nameLast").val(),
+		spouse_nameFirst:$("#spouse_nameFirst").val(),
+		spouse_nameMiddle:$("#spouse_nameMiddle").val(),
+		spouse_nameLast:$("#spouse_nameLast").val()},
        dataType: 'json',
        success: function (res) {
                    console.log(res);
                    if(res[0][0] != "None")
                    {
 			
-			table = '<table border="1">';
-			
-			for(i=0; i<res.length; i++)
-			{
-				
-				row = res[i];
-				table += "<tr>";
-				for(j=0; j<row.length; j++)
-				
-				{
-					
-					table += "<td>" + row[j] + "</td>";
-				
-				}
-				table += "</tr>";
-			
-			}
-			table += "</table>";
-			$("#target").html(table);
+			answer = 'SET'
+			$("#target").html(answer);
 		} //end if
 	}
 	});
