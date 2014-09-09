98d97481c8167209d1883f735fdf56a1d30576a7
Raphael Jose Balonga <raphaeljose.balonga@gmail.com>
2014-09-08 19:15:29 -0700

    Debugged errors.

--- scripts/addorgs.py
+++ scripts/addorgs.py
@@ -1,6 +1,6 @@
 from dosql import *
 import cgi
-import json
+import simplejson as json
 
 def index(req, stud_id, org_Name, org_Pos, org_AcYr, stud_aA_cA, stud_schGra):
     stud_id = cgi.escape(stud_id)
