66c88cc363a381ef761e035d4d9a5fcd5ed4b9b1
Pio Baliton Pentin <acheche0910@yahoo.com.ph>
2014-09-14 14:22:09 +0800

    edited header.py

--- scripts/header.py
+++ scripts/header.py
@@ -2,7 +2,7 @@ from dosql import *
 import cgi
 import json
 
-def index(req, stud_id, nameFirst, nameMid, nameLast, birthdate, birthMonth, birthYear, homeAdress, father_nameFirst, father_nameMiddle,father_nameLast, mother_nameFirst, mother_nameMiddle, mother_nameLast):
+def index(req, stud_id, nameFirst, nameMid, nameLast, birthdate, birthMonth, birthYear, homeAdress, father_nameFirst, father_nameMiddle,father_nameLast, mother_nameFirst, mother_nameMiddle, mother_nameLast, dissertation, special_project, thesis_title):
   
   stud_id = cgi.escape(stud_id)
   nameFirst = cgi.escape(nameFirst)
@@ -18,9 +18,12 @@ def index(req, stud_id, nameFirst, nameMid, nameLast, birthdate, birthMonth, bir
   mother_nameFirst = cgi.escape(mother_nameFirst)
   mother_nameMiddle = cgi.escape(mother_nameMiddle)
   mother_nameLast = cgi.escape(mother_nameLast)
+  dissertation = cgi.escape(dissertation)
+  special_project = cgi.escape(special_project)
+  thesis_title = cgi.escape(thesis_title)
 
   x = doSql()
-  student = x.execqry("select * from get_header_perid('" + stud_id + "', '" + nameFirst + "', '" + nameMid + "', '" + nameLast + "', '" + birthdate + "', '" + birthMonth + "', '" + birthYear + "', '" + homeAdress + "', '" + father_nameFirst + "', '" + father_nameMiddle + "', '" + father_nameLast + "', '" + mother_nameFirst + "', '" + mother_nameMiddle + "', '" + mother_nameLast + "', '" + guardian_nameFirst + "', '" + guardian_nameMiddle + "', '" + guardian_nameLast + "', '" + spouse_nameFirst + "', '" + spouse_nameMiddle + "', '" + spouse_nameLast + "');", True)
+  student = x.execqry("select * from get_header_perid('" + stud_id + "', '" + nameFirst + "', '" + nameMid + "', '" + nameLast + "', '" + birthdate + "', '" + birthMonth + "', '" + birthYear + "', '" + homeAdress + "', '" + father_nameFirst + "', '" + father_nameMiddle + "', '" + father_nameLast + "', '" + mother_nameFirst + "', '" + mother_nameMiddle + "', '" + mother_nameLast + "', '" + guardian_nameFirst + "', '" + guardian_nameMiddle + "', '" + guardian_nameLast + "', '" + spouse_nameFirst + "', '" + spouse_nameMiddle + "', '" + spouse_nameLast + "', '" + dissertation + "', '" + special_project + "', '" + thesis_title + "');", True)
   result = []
   for stud in student:
     stringed = map(str, stud)
