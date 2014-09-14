038d574934d78d9389725bf0a5d13db60b56508f
Kring2 <cristylorainefuerzas@gmail.com>
2014-09-14 21:18:54 +0800

    added files and functions to make edition possible

---
+++ scripts/getstudinfoperid.py
@@ -0,0 +1,15 @@
+from dosql import *
+import cgi
+import json
+
+def index(req, stud_id):
+    stud_id = cgi.escape(stud_id)
+    x = doSql()
+    rets = x.execqry("select * from getstudinfoperid('" + stud_id+"');", False)
+    result = []
+    for ret in rets:
+        stringed = map(str, ret)
+        result.append(stringed)
+    
+
+    return json.dumps(result)
