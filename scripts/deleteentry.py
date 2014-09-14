432dea498ffcabf799846a14c1b6b41cc13215f4
Lorie Anne M. Castillano <clorieanne@gmail.com>
2014-09-14 14:48:57 +0800

    modified deleteentry.py

--- scripts/deleteentry.py
+++ scripts/deleteentry.py
@@ -8,10 +8,6 @@ def index(req, stud_id):
 
   x = doSql()
   studs = x.execqry("select * from del_studid_hs('" + stud_id + "');", True)
-  studs1 = x.execqry("select * from del_studinfo('" + stud_id + "');", True)
-  studs2 = x.execqry("select * from del_confirm('" + stud_id + "');", True)
-  studs3 = x.execqry("select * from del_orgs('" + stud_id + "');", True)
-  studs4 = x.execqry("select * from del_parents('" + stud_id + "');", True)
   result = []
 
   for stud in studs:
