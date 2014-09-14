b4c4db86622e1f5d31afcee0404dceb053ce0cdb
Kring2 <cristylorainefuerzas@gmail.com>
2014-09-14 14:02:45 +0800

    added a new function [editform()] to allinfo.js

--- javascript/allinfo.js
+++ javascript/allinfo.js
@@ -54,4 +54,55 @@ function addstudinfo()
     });
 }
 
-
+function editform()
+{
+  $.ajax({
+	url: siteloc + scriptloc + "getstudinfo.py",
+	data: {stud_id:$("#stud_id").val()}, 
+	dataType: 'json',
+	success: function(res){
+		console.log(res);
+		if(res[0][0] != "None")
+                    {
+			 rets = '<div class="edit">';
+			rets += '<input type="character" value="'+res[0][0]+'" class="fill" id="stud_id" required="highly"></br>'
+				+ '<input type="text" value="'+ res[0][1] +'" class="fill" id="college" required="highly"></br>'
+				+ '<input type="text" value="'+res[0][2]+'" class="fill" id="course" required="highly"></br>'
+				+ '<input type="text" value="'+res[0][2]+'" class="fill" id="nameFirst" required="highly"></br>'
+				+ '<input type="text" value="'+res[0][2]+'" class="fill" id="nameMid" required="highly"></br>'
+				+ '<input type="text" value="'+res[0][2]+'" class="fill" id="nameLast" required="highly"></br>'
+				+ '<input type="text" value="'+res[0][2]+'" class="fill" id="nickname" required="highly"></br>'
+				+ '<input type="text" value="'+res[0][2]+'" class="fill" id="gender" required="highly"></br>'
+				+ '<input type="int" value="'+res[0][2]+'" class="fill" id="birthdate" required="highly"></br>'
+				+ '<input type="int" value="'+res[0][2]+'" class="fill" id="birthMonth" required="highly"></br>'
+				+ '<input type="int" value="'+res[0][2]+'" class="fill" id="birthYear" required="highly"></br>'
+				+ '<input type="int" value="'+res[0][2]+'" class="fill" id="age" required="highly"></br>'
+				+ '<input type="text" value="'+res[0][2]+'" class="fill" id="contactNum" required="highly"></br>'
+				+ '<input type="text" value="'+res[0][2]+'" class="fill" id="homeAddress" required="highly"></br>'
+				+ '<input type="text" value="'+res[0][2]+'" class="fill" id="father_nameFirst" required="highly"></br>'
+				+ '<input type="text" value="'+res[0][2]+'" class="fill" id="father_nameMiddle" required="highly"></br>'
+				+ '<input type="text" value="'+res[0][2]+'" class="fill" id="father_nameLast" required="highly"></br>'
+				+ '<input type="text" value="'+res[0][2]+'" class="fill" id="mother_nameFirst" required="highly"></br>'
+				+ '<input type="text" value="'+res[0][2]+'" class="fill" id="mother_nameMiddle" required="highly"></br>'
+				+ '<input type="text" value="'+res[0][2]+'" class="fill" id="mother_nameLast" required="highly"></br>'
+				+ '<input type="text" value="'+res[0][2]+'" class="fill" id="guardian_nameFirst" required="highly"></br>'
+				+ '<input type="text" value="'+res[0][2]+'" class="fill" id="guardian_nameMiddle" required="highly"></br>'
+				+ '<input type="text" value="'+res[0][2]+'" class="fill" id="guardian_nameLast" required="highly"></br>'
+				+ '<input type="text" value="'+res[0][2]+'" class="fill" id="spouse_nameFirst" required="highly"></br>'
+				+ '<input type="text" value="'+res[0][2]+'" class="fill" id="spouse_nameMiddle" required="highly"></br>'
+				+ '<input type="text" value="'+res[0][2]+'" class="fill" id="spouse_nameLast" required="highly"></br>'
+				+ '<input type="text" value="'+res[0][2]+'" class="fill" id="org_Name" required="highly"></br>'
+				+ '<input type="text" value="'+res[0][2]+'" class="fill" id="org_Pos" required="highly"></br>'
+				+ '<input type="text" value="'+res[0][2]+'" class="fill" id="org_AcYr" required="highly"></br>'
+				+ '<input type="text" value="'+res[0][2]+'" class="fill" id="stud_aA_cA" required="highly"></br>'
+				+ '<input type="text" value="'+res[0][2]+'" class="fill" id="stud_schGra" required="highly"></br>'
+				+ '<input type="text" value="'+res[0][2]+'" class="fill" id="dissertation" required="highly"></br>'
+				+ '<input type="text" value="'+res[0][2]+'" class="fill" id="special_project" required="highly"></br>'
+				+ '<input type="text" value="'+res[0][2]+'" class="fill" id="thesis_title" required="highly"></br>'
+				+ '<input type="float" value="'+res[0][3]+'" class="fill" id="status" required="highly"></br>';
+			rets += '<button onclick="additem();">Add Item</button>'  + '</div>';
+					  $("#target").html(table); 
+				  } // end if
+		}
+	});
+}
