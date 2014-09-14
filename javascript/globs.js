fee6a01d1ca3782b911e093675d70ffd312e383a
Kring2 <cristylorainefuerzas@gmail.com>
2014-09-14 15:29:11 +0800

    just checking

--- javascript/globs.js
+++ javascript/globs.js
@@ -267,7 +267,7 @@ function addorgs()
 function addstudinfo()
 {
 	$.ajax({
-      url: siteloc + scriptloc + "addallinfo.py",
+      url: siteloc + scriptloc + "addstudent.py",
       data: {stud_id : $("#stud_id").val(),
              college : $("#college").val(),
              course  : $("#course").val(),
