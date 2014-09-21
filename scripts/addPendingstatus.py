aa38b0bbe55f97e8f333c7581201d62c375fa688
Kring2 <cristylorainefuerzas@gmail.com>
2014-09-21 20:12:19 +0800

    added new python files for the new database

---
+++ scripts/addPendingstatus.py
@@ -0,0 +1,16 @@
+from dosql import *
+import cgi
+import json
+
+def index(req, stud_id):
+    stud_id =  cgi.escape(stud_id)
+    x = doSql()
+
+    studs = x.execqry("select * from setstatus('" + stud_id + "', 'Pending');", True)
+    result = []
+
+    for stud in studs:
+        stringed = map(str, stud)
+        result.append(stringed)
+
+    return json.dumps(result)
---
+++ scripts/personal_info.py
@@ -0,0 +1,43 @@
+from dosql import *
+import cgi
+import json
+
+def index(req, stud_id, nameFirst, nameMid, nameLast, nickname, gender, 
+			birthdate, birthMonth, birthYear, age, 
+			contactNum, homeAddress, father_nameFirst, 
+			father_nameMiddle, father_nameLast, mother_nameFirst,  
+			mother_nameMiddle, mother_nameLast, guardian_nameFirst,
+			guardian_nameMiddle, guardian_nameLast, spouse_nameFirst, 
+			spouse_nameMiddle, spouse_nameLast):
+  stud_id = cgi.escape(stud_id)
+  nameFirst = cgi.escape(nameFirst)
+  nameMid = cgi.escape(nameMid)
+  nameLast = cgi.escape(nameLast)
+  nickname = cgi.escape(nickname)
+  gender = cgi.escape(gender)
+  birthdate = cgi.escape(birthdate)
+  birthMonth = cgi.escape(birthMonth)
+  birthYear = cgi.escape(birthYear)
+  age = cgi.escape(age)
+  contactNum = cgi.escape(contactNum)
+  homeAddress = cgi.escape(homeAddress)
+  father_nameFirst = cgi.escape(father_nameFirst)
+  father_nameMiddle = cgi.escape(father_nameMiddle)
+  father_nameLast = cgi.escape(father_nameLast)
+  mother_nameFirst = cgi.escape(mother_nameFirst)
+  mother_nameMiddle = cgi.escape(mother_nameMiddle)
+  mother_nameLast = cgi.escape(mother_nameLast)
+  guardian_nameFirst = cgi.escape(guardian_nameFirst)
+  guardian_nameMiddle = cgi.escape(guardian_nameMiddle)
+  guardian_nameLast = cgi.escape(guardian_nameLast)
+  spouse_nameFirst = cgi.escape(spouse_nameFirst)
+  spouse_nameMiddle = cgi.escape(spouse_nameMiddle)
+  spouse_nameLast = cgi.escape(spouse_nameLast)
+  x = doSql()
+  student = x.execqry("select * from regstudent('" + stud_id +  "', '" + nameFirst + "', '" + nameMid + "', '" + nameLast + "', '" + nickname + "', '" + gender + "', " + birthdate + ", " + birthMonth + ", " + birthYear + ", '" + age + "', '" + contactNum + "', '" + homeAddress + ", " + father_nameFirst + "', '" + father_nameMiddle + "', '" + father_nameLast + "', '" + mother_nameFirst + "', '" + mother_nameMiddle + "', '" + mother_nameLast + "', '" + guardian_nameFirst + "', '" + guardian_nameMiddle + "', '" + guardian_nameLast + "', '" + spouse_nameFirst + "', '" + spouse_nameMiddle + "', '" + spouse_nameLast + "');", True)
+  result = []
+  for stud in student:
+    stringed = map(str, stud)
+    result.append(stringed)
+
+  return json.dumps(result)
