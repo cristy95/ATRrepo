78a7a7cdfe3bb36d5fb49f67b38a5a161c34aa31
Raphael Jose Balonga <raphaeljose.balonga@gmail.com>
2014-09-21 14:12:17 -0700

    Modified Confirming function

--- javascript/globs.js
+++ javascript/globs.js
@@ -103,46 +103,6 @@ function fetchconfirm(stud_id)
     });
 }
 
-//josh
-function fetchconfirmlist(status)
-{
-  $.ajax({
-      url: siteloc + scriptloc + "getgradesheet.py",
-      data: {status:$("#status").val())},
-      dataType: 'json',
-      success: function (res) {
-                  console.log(res);
-                  if(res[0][0] != "None")
-                  {
-				      table = '<div class="table-responsive">';
-					  table += '<table class="table table-condensed">';
-					  table += '<thead>' +
-					           '<tr>' +
-							     '<th>ID No.</th>' +
-								 '<th>Prelim</th>' +
-								 '<th>Midterm</th>' +
-								 '<th>Finals</th>' +
-							   '</tr>' +
-					           '</thead>';
-					  table += "<tbody>";		   
-					  for (i = 0; i < res.length; i++)
-					  {
-						  row = res[i];
-						  table += "<tr>";
-						  for (j = 0; j < row.length; j++)
-						  {
-							  table += "<td>" + row[j] + "</td>";
-						  }
-						  table += "</tr>";
-					  }
-					  table += "</tbody>";
-					  table += "</table>";
-					  table += "</div>";
-					  $("#target").html(table); 
-				  } // end if
-              }
-    });
-}
 
 //josh
 function fetchorgs_perid(stud_id)
