131bc4a49679e9a50098aa3a410000da23b8f190
Pio Baliton Pentin <acheche0910@yahoo.com.ph>
2014-09-24 00:00:59 +0800

    merge my header/template javascript code to main globs and edited my header/template javascript file

--- javascript/globs.js
+++ javascript/globs.js
@@ -446,3 +446,39 @@ function editform()
 		}
 	});
 }
+
+function getparents_perid(stud_id)
+{
+   $.ajax({
+       
+	   url: siteloc + scriptloc + "header.py",
+       data: {stud_id:stud_id},
+       dataType: 'json',
+       success: function (res) {
+                   console.log(res);
+                   if(res[0][0] != "None")
+                   {
+			
+			table = '<table border="1">';
+			
+			for(i=0; i<res.length; i++)
+			{
+				
+				row = res[i];
+				table += "<tr>";
+				for(j=0; j<row.length; j++)
+				
+				{
+					
+					table += "<td>" + row[j] + "</td>";
+				
+				}
+				table += "</tr>";
+			
+			}
+			table += "</table>";
+			$("#target").html(table);
+		} //end if
+	}
+	});
+}
--- javascript/header.js
+++ javascript/header.js
@@ -1,43 +1,38 @@
 var siteloc = "http://localhost/ATRrepo";
 var scriptloc = "/scripts/"
 
-function getheader()
+
+function getparents_perid(stud_id)
 {
    $.ajax({
        
 	   url: siteloc + scriptloc + "header.py",
-       data: {stud_id:$("#stud_id").val(),
-	    father_nameFirst:$("#nameFirst").val(),
-	    father_nameFirst:$("#nameMid").val(),
-	    father_nameFirst:$("#nameLast").val(),
-	    father_nameFirst:$("#birthdate").val(),
-	    father_nameFirst:$("#birthMonth").val(),
-	    father_nameFirst:$("#birthYear").val(),
-	    father_nameFirst:$("#homeAdress").val(),
-		father_nameFirst:$("#father_nameFirst").val(),
-		father_nameMiddle:$("#father_nameMiddle").val(),
-		father_nameLast:$("#father_nameLast").val(),
-		mother_nameFirst:$("#mother_nameFirst").val(),
-		mother_nameMiddle:$("#mother_nameMiddle").val(),
-		mother_nameLast:$("#mother_nameLast").val(),
-		guardian_nameFirst:$("#guardian_nameFirst").val(),
-		guardian_nameMiddle:$("#guardian_nameMiddle").val(),
-		guardian_nameLast:$("#guardian_nameLast").val(),
-		spouse_nameFirst:$("#spouse_nameFirst").val(),
-		spouse_nameMiddle:$("#spouse_nameMiddle").val(),
-		spouse_nameLast:$("#spouse_nameLast").val()},
-		spouse_nameLast:$("#dissertation").val()},
-		spouse_nameLast:$("#special_project").val()},
-		spouse_nameLast:$("#thesis_title").val()},
-		
+       data: {stud_id:stud_id},
        dataType: 'json',
        success: function (res) {
                    console.log(res);
                    if(res[0][0] != "None")
                    {
 			
-			answer = 'SET'
-			$("#target").html(answer);
+			table = '<table border="1">';
+			
+			for(i=0; i<res.length; i++)
+			{
+				
+				row = res[i];
+				table += "<tr>";
+				for(j=0; j<row.length; j++)
+				
+				{
+					
+					table += "<td>" + row[j] + "</td>";
+				
+				}
+				table += "</tr>";
+			
+			}
+			table += "</table>";
+			$("#target").html(table);
 		} //end if
 	}
 	});
