8b7936b789f02e7c048cfe4f3ee13093a8ee51ae
Kring2 <cristylorainefuerzas@gmail.com>
2014-09-29 00:42:28 +0800

    modified files for getting colleges and courses

--- scripts/getcourses.py
+++ scripts/getcourses.py
@@ -2,12 +2,14 @@ from dosql import *
 import cgi
 import json
 
-def index(req):
-	x = doSql()
-	rets = x.execqry("select * from courses")
+def index(req, college_id_fk):
+  x = doSql()
+  rets = x.execqry("select course_id, course_name from courses \
+			where college_id_fk = " + college_id_fk , 'False')
 
-	for ret in rets:
-		stringed = map(str, ret)
-		result.append(stringed)
+  result = []
+  for ret in rets:
+    stringed = map(str, ret)
+    result.append(stringed)
 
-	return json.dumps(result)
+  return json.dumps(result)
