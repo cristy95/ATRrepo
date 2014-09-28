8a985a7e2bc6b8ff010a7e136da34dc9213f1221
thundervoice <dmbbandola@gmail.com>
2014-09-28 20:06:00 +0800

    added yeah

---
+++ scripts/getcourses.py
@@ -0,0 +1,13 @@
+from dosql import *
+import cgi
+import json
+
+def index(req):
+	x = doSql()
+	rets = x.execqry("select * from courses")
+
+	for ret in rets:
+		stringed = map(str, ret)
+		result.append(stringed)
+
+	return json.dumps(result)
